41f1de84e23536fc390cc3d1df8c415fa81de7b2
LieRossi <40723203+LieRossi@users.noreply.github.com>
2019-02-10 20:40:26 -0200

    vibraçao

---
+++ ElieteRossi_UC7_A1_AlienCity_Mobile/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	public float MoveSpeed;
+	public float RotationSpeed;
+	CharacterController cc;
+	private Animator anim;
+	protected Vector3 gravidade = Vector3.zero;
+	protected Vector3 move = Vector3.zero;
+	private bool jump = false;
+    
+   
+    public float moveSpeed = 1f;
+    public Joystick joystick;
+
+    private Vector3 auxV;
+    private Vector3 auxH;
+    private Vector3 V;
+    private Vector3 H;
+    private Vector3 movement;
+
+
+
+    void Start()
+	{
+		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator>();
+		anim.SetTrigger("Parado");
+	}
+
+    void Update()
+    {
+        float auxV = Input.GetAxis("Vertical");
+        float auxH = Input.GetAxis("Horizontal");
+        Vector3 move = auxV * transform.TransformDirection(Vector3.forward) * MoveSpeed;// move para frente/tras
+        transform.Rotate(new Vector3(0, auxH * RotationSpeed * Time.deltaTime, 0));// rotaciona para os lados
+
+        if (!cc.isGrounded) {
+            gravidade += Physics.gravity * Time.deltaTime;
+        }
+        else
+        {
+            gravidade = Vector3.zero;
+            if (jump)
+            {
+                gravidade.y = 6f;
+                jump = false;
+            }
+        }
+        move += gravidade;
+        cc.Move(move * Time.deltaTime);
+        Anima();
+        //BotaoPular();
+        
+    
+
+          Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+        if (moveVector != Vector3.zero)
+        {
+             float V = joystick.Vertical;
+             float H = joystick.Horizontal;
+ 
+            transform.Translate(moveVector * Time.deltaTime * MoveSpeed);//move para frente/tras
+            transform.Rotate(new Vector3(0, H * RotationSpeed * Time.deltaTime, 0)*MoveSpeed);// rotaciona para os lados
+
+
+        }
+    }
+
+    void Anima()
+    {
+        if (!Input.anyKey)
+        {
+            anim.SetTrigger("Parado");
+        }
+        else
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                anim.SetTrigger("Pula");
+                jump = true;
+            }
+            else
+            {
+                anim.SetTrigger("Corre");
+
+            }
+        }
+    }
+
+    /*void BotaoPular()
+    {
+        if (Input.GetButtonDown("BotaoPular"))
+        {
+            anim.SetTrigger("Pula");
+            jump = true;
+        }
+    }*/
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pedra") || (other.gameObject.CompareTag("tiro")))
+        {
+            Handheld.Vibrate();
+        }
+    }
+
+}
+
+
+
---
+++ ElieteRossi_UC7_A1_AlienCity_Mobile/Assets/Scripts/botaoController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botaoController : MonoBehaviour {
+    public float forca;
+    public float distanciaDoRaio;
+    public Rigidbody Alien_R;
+    private bool cliqueiNoBotao, estouNoChao;
+    private Animator anim;
+    CharacterController cc;
+    private bool jump = false;
+    protected Vector3 gravidade = Vector3.zero;
+    protected Vector3 move = Vector3.zero;
+
+
+
+
+    // Use this for initialization
+    void Start() {
+        anim = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
+        anim.SetTrigger("Parado");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+        RaycastHit raio;
+
+        if (Physics.Raycast(transform.position, -transform.up, out raio, distanciaDoRaio))//se colidiu...
+        {
+            if (raio.collider)
+            {
+                estouNoChao = true;
+            }
+        }
+        else //se não colidiu
+        {
+            estouNoChao = false;
+        }
+        if (estouNoChao)
+
+        {
+            if (cliqueiNoBotao == true)
+            {
+               
+                Alien_R.AddForce(Vector3.up * forca * Time.deltaTime);
+                //gravidade += Physics.gravity * Time.deltaTime;
+                //gravidade.y = 6f;
+                //move += gravidade;
+                //cc.Move(move * Time.deltaTime);
+
+                cliqueiNoBotao = false;
+            }
+            else
+            {
+                cliqueiNoBotao = false;
+            }
+        }
+    }
+
+    public void BotaoPular(bool cliquei)
+    {
+        cliqueiNoBotao = true;
+        anim.SetTrigger("Pula");
+        jump = true;
+        gravidade += Physics.gravity * Time.deltaTime;
+        gravidade.y = 10f;
+        move += gravidade;
+        cc.Move(move * Time.deltaTime);
+        cliqueiNoBotao = false;
+        jump = false;
+    }
+
+      
+
+}
